4c3fa09ea7ea7a5795639e1bd01abc1b69477c24
Sesu8642 <45859254+Sesu8642@users.noreply.github.com>
2022-12-16 20:57:06 +0100

    Initial commit: generated Spring Boot app

---
+++ src/main/java/de/sesu8642/wwchallenge/WwChallengeApplication.java
@@ -0,0 +1,13 @@
+package de.sesu8642.wwchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sesu8642/wwchallenge/WwChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sesu8642.wwchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
